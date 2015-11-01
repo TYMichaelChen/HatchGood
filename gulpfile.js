dc5a80f7e601aab86c05c145529895563eb54d7b
Michael Chen <miketychen@gmail.com>
2015-11-01 09:56:31 -0600

    autoprefix and minify

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,9 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
-
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+var rename = require('gulp-rename');
 // Lint Task
 gulp.task('lint', function() {
     return gulp.src('js/*.js')
@@ -18,7 +20,11 @@ gulp.task('lint', function() {
 gulp.task('sass', function() {
     return gulp.src('app/scss/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('public/stylesheets'));
+        .pipe(autoprefixer('last 2 version'))
+        .pipe(gulp.dest('public/stylesheets'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss())
+        .pipe(gulp.dest('public/stylesheets/min'));
 });
 
 // Concatenate & Minify JS
