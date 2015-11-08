b8650da8d405314a334cab4d1d4c8bece065a3af
Michael Chen <miketychen@gmail.com>
2015-11-08 12:05:07 -0600

    fix frontend components

--- app/routes/api.js
+++ app/routes/api.js
@@ -13,7 +13,7 @@ module.exports = function(app){
 	});
 
 	app.get('*',function(req,res){
-		res.sendfile('./public/dist/views/index.html');
+		res.sendFile('./public/dist/views/index.html');
 	})
 
 }
