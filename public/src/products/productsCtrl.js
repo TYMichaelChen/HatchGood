88067a03ead7ce4a34598b428ac747fa1bc8951e
Michael Chen <miketychen@gmail.com>
2015-12-18 15:45:25 -0800

    modals

--- public/src/products/productsCtrl.js
+++ public/src/products/productsCtrl.js
@@ -1,10 +1,11 @@
 angular.module('productCtrl', [])
 
 
-.controller('productController', function($scope,$log) {
+.controller('productController', function($scope,$uibModal,$log) {
 	$scope.myInterval = 5000;
 	$scope.noWrapSlides = false;
 	$scope.tab=true;
+  //carousel images
 	var slides = $scope.slides = [
 		{
 	      image: '../assets/images/product/pendant/1.jpg'
@@ -19,6 +20,7 @@ angular.module('productCtrl', [])
 	      image: '../assets/images/product/pendant/4.jpg'
 	    }
 	];
+  //Gallery images
 	$scope.gallery = [
     {thumb: '../assets/images/product/pendant/1.jpg', img: '../assets/images/product/pendant/1.jpg', description: 'Image 1'},
     {thumb: '../assets/images/product/pendant/2.jpg', img: '../assets/images/product/pendant/2.jpg', description: 'Image 2'},
@@ -27,18 +29,57 @@ angular.module('productCtrl', [])
     {thumb: '../assets/images/product/pendant/5.jpg', img: '../assets/images/product/pendant/5.jpg', description: 'Image 5'},
     {thumb: '../assets/images/product/pendant/6.jpg', img: '../assets/images/product/pendant/6.jpg', description: 'Image 6'},
   ];
+  //accordion for description
+  $scope.accordionStatus = {
+    description:true,
+    material:false,
+    source:false,
+    ship:false
+  };
+  //progress bar
+  $scope.progress = {
+    value:4,
+    max:10,
+  };
+  $scope.progress.percent=($scope.progress.value-1.55)/$scope.progress.max*100;
+  $scope.openHatchModal = function() {
+    var modalInstance = $uibModal.open({
+      animation: true,
+      templateUrl: 'products/partials/products/hatchModal.html',
+      controller: 'hatchModalCtrl',
+    });
+  };
+  $scope.openDesignerModal = function(id) {
 
-    $scope.accordionStatus = {
-    	description:true,
-    	material:false,
-    	source:false,
-    	ship:false
-    };
-    $scope.progress = {
-        value:4,
-        max:10,
-    };
-    $scope.progress.percent=($scope.progress.value-1.55)/$scope.progress.max*100;
+    var modalInstance = $uibModal.open({
+      animation: true,
+      templateUrl: 'products/partials/products/designerModal.html',
+      controller: 'designerModalCtrl',      
+      windowClass: 'designerModalWindow',
+      resolve:{
+        data: function(){
+          return id;
+        }
+      }
+    });
+  };
+
+})
+//hatch modal controller 
+.controller('hatchModalCtrl', function ($scope, $uibModalInstance) {
+  $scope.close = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+  $scope.submit = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+})
+//designer modal controller 
+.controller('designerModalCtrl', function ($scope, $uibModalInstance, $log, data) {
+  $scope.designerName = data;
+  $scope.close = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
 })
 //sidebar scroll function
 .directive("sidebar", function ($window,$log) {
@@ -54,20 +95,22 @@ angular.module('productCtrl', [])
         });
     };
 })
-//Override UI Bootstrap Carousel Directive's Template
 .config(['$provide', Decorate]);
   function Decorate($provide) {
+
+  //Override ngGallery Directive's Template
   $provide.decorator('ngGalleryDirective', function($delegate) {
     var directive = $delegate[0];  
     directive.templateUrl = "products/partials/products/gallery.tpl.html";
     
     return $delegate;
   });
+
+  //Override UI Bootstrap Carousel Directive's Template
   $provide.decorator('uibCarouselDirective', function($delegate) {
     var directive = $delegate[0];  
     directive.templateUrl = "products/partials/products/carousel.tpl.html";
     
     return $delegate;
   });
-}
-  
+};
